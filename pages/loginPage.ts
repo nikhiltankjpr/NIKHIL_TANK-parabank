6ba20ce393ed06b30359b46e4f1c4436dcf7b560
nikhiltankjpr <nikhiltankjpr@gmail.com>
2026-06-15 21:38:41 +0530

    new6

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -1,23 +1,14 @@
-import {Page,expect} from '@playwright/test';
+
+
+import{Page}from'@playwright/test';
+
 export class LoginPage{
 constructor(private page:Page){}
 async login(username:string,password:string){
+await this.page.getByRole('link',{name:'Register'}).click();
 await this.page.locator('input[name="username"]').fill(username);
 await this.page.locator('input[name="password"]').fill(password);
 await this.page.getByRole('button',{name:'Log In'}).click();
-await expect(this.page.getByRole('link',{name:'Open New Account'})).toBeVisible();
 }
 }
-
-// import{Page}from'@playwright/test';
-
-// export class LoginPage{
-// constructor(private page:Page){}
-// async login(username:string,password:string){
-// await this.page.getByRole('link',{name:'Register'}).click();
-// await this.page.locator('input[name="username"]').fill(username);
-// await this.page.locator('input[name="password"]').fill(password);
-// await this.page.getByRole('button',{name:'Log In'}).click();
-// }
-// }
 
